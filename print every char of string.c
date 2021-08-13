ad998dccafe960905c61bc4b4b5965c08d264dc9
devansh888 <84080583+devansh888@users.noreply.github.com>
2021-08-13 17:02:14 +0530

    Add files via upload
    Program to print every character of a string.

---
+++ print every char of string.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<cs50.h>
+#include<string.h>
+
+
+int main(void)
+{
+    string s = get_string("S: ");
+
+    int n = strlen(s);
+
+    for(int i = 0; i<n; i++)
+    {
+        printf("%c\n", s[i]);
+    }
+}
